34dab9c7052295813d9dd95aeb3b8b6e1a1dec7b
Oleksandr <aleksandr.mpro@gmail.com>
2024-09-15 19:25:39 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,55 +1,57 @@
 import React, { Component } from "react";
 import "./App.css";
-import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
-import PokemonForm from "components/PokemonForm";
-import PokemonInfo from "components/PokemonInfo";
+import Searchbar from "./components/Searchbar";
+import ImageGallery from "./components/ImageGallery";
+import Button from "./components/Button";
+import * as API from "../src/services/pixabay-api";
+import { Hourglass } from 'react-loader-spinner';
 
 
 class App extends Component {
   state = {
-    pokemonName: null,
+    pictureName: "",
+    pictures: [],
+    page: 1,
+    totalHits:0,
+    loading: false,
   };
+  
 
-  handleFormSubmit = (pokemonName) => {
-    console.log(pokemonName);
-    this.setState({pokemonName})
+  handleClickButton=async()=>{
+    const newpage=this.state.page+1;
+    console.log(newpage);
+    try {
+      const apiPictures = await API.getPictures(this.state.pictureName, newpage);
+      this.setState((state)=>({ pictures: [...state.pictures, ...apiPictures.hits ], page: newpage}));
+      console.log(apiPictures.hits );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+ 
+  handleFormSubmit = async (pictureName) => {
+    console.log(pictureName);
+    this.setState({ pictureName,  page: 1, loading: true});
+
+    try {
+      const apiPictures = await API.getPictures(pictureName, 1);
+      this.setState({ pictures: apiPictures.hits, totalHits: apiPictures.totalHits, loading: false});
+      console.log(apiPictures.hits );
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   render() {
     return (
-      <div style={{ maxWidth: 1170, margin: "0 auto", padding: 20 }}>
-        <PokemonForm onSubmit={this.handleFormSubmit} />
-        <PokemonInfo pokemonName = {this.state.pokemonName}/>
-        <ToastContainer autoClose={3000}/>
-      </div>
+      <>
+        <Searchbar onSubmit ={this.handleFormSubmit}/>
+        {this.state.loading&&<Hourglass visible={true} wrapperStyle={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }} wrapperClass="my-custom-loader"/>}
+        {this.state.pictures&&<ImageGallery pictures={this.state.pictures}/>}
+        {this.state.pictures.length > 0 && this.state.pictures.length < this.state.totalHits&&<Button onClick={this.handleClickButton}/>}
+      </>
     );
   }
 }
-
-// class App extends Component {
-//   state = {
-//     pokemon: null,
-//     loading: false,
-//   };
-//   componentDidMount() {
-//     this.setState({loading: true});
-//     fetch("https://pokeapi.co/api/v2/pokemon/ditto")
-//       .then((res) => res.json())
-//       .then(pokemon=>this.setState({pokemon})).finally(()=>this.setState({loading: false}));
-//   }
-
-//   render() {
-//     return (
-//       <div style={{ maxWidth: 1170, margin: "0, auto", padding: 20 }}>
-//       {this.state.loading&&<h1>Загружаем...</h1>}
-//         {this.state.pokemon && (
-//           <div>{this.state.pokemon.name}</div>
-//         )}
-//         {/* <ToastContainer autoClose={3000} /> */}
-//       </div>
-//     );
-//   }
-// }
 
 export default App;
